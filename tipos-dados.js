f555f675a33abedcd80c152460e65142abc5a010
AlanPaulinoDaSilva <alanpaulino_1@hotmail.com>
2021-05-15 16:48:03 -0300

    parte 2 aula java scipt

---
+++ tipos-dados.js
@@ -0,0 +1,19 @@
+const inteiro=10;
+const decimal=10.5;
+const bool= false;
+const nome= "Alan"
+let copo=null;
+let televisao;
+let pessoa={
+    nome:"Alan"
+}
+const symb=Symbol()
+
+console.log("Tipo da variavel",typeof inteiro);
+console.log("Tipo da variavel",typeof decimal);
+console.log("Tipo da variavel",typeof bool); 
+console.log("Tipo da variavel",typeof nome);
+console.log("Tipo de variavel",typeof copo);
+console.log("Tipo da variavel", typeof televisao);
+console.log("Tipo da variavel", typeof pessoa);
+console.log("tipo de variavel", typeof symb);
---
+++ variaveis-constantes.js
@@ -0,0 +1,4 @@
+var cor="branca"//"var" variavel global
+let nome="Alan"//"let" variavel de escopo local
+const empresa="Devaria";//"const" variavel constante, não muda
+console.log(cor,nome);
